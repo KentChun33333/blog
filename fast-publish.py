f41afee24f09e90aa3473c036a73095d9fde96a2
KentChun33333 <kentchun33333@gmail.com>
2017-01-03 13:42:44 +0800

    RL-01-MDP and fast publish and regular Note Asset

--- fast-publish.py
+++ fast-publish.py
@@ -2,24 +2,23 @@
 
 """
 ### Author : Kent Chiu
+- Note Identity Encoding Regulation : className-xx-name.ipynb
+ - className should be correnspoding to content/folderName/
+ - xx and name should be in one-to-one relation
+ - name must not contain - symbol
+ - xx in [00,99] 
+ - where we call className-xx-name == NoteID
 
-- Note Identity Encoding Regulation : **className-xx-name.ipynb**
- - **className** should be correnspoding to content/**folderName**/*
- - **xx** and **name** should be in one-to-one relation
- - **name** with out - 
- - **xx** in [00,99] 
- - where we call **className-xx-name == NoteID**
-
-- Asset Identity Encoding Regulation : **className-xx-name-yy** == **NoetID-yy**
- - **yy** in [00,99]
- - **yy** is the number on the condition of **Asset Type**, for example : 
+- Asset Identity Encoding Regulation : className-xx-name-yy == NoetID-yy
+ - yy in [00,99]
+ - yy is the number on the condition of Asset Type, for example : 
    - RL-01-MDP-00.jpg
    - RL-01-MDP-00.avi
  
 - Asset Type 
  - img (jpg | png) 
- - video (avi with h264 codec)
- - audio (mp3)
+ - video (avi with h264 codec) better via youtube or other CDN
+ - audio (mp3) better via youtube or other CDN
  
 """
 
@@ -37,7 +36,7 @@ def get_asset( dirname ,NoteID):
     assert os.path.isdir('output/image') == True
     file_list = os.listdir(dirname+'/image')
     for item in file_list :
-        if item.split('.')[-1] not in ('jpg','png','avi','jepg','bmp'):
+        if item.split('.')[-1] not in ('jpg','png','jepg','bmp','avi'):
             continue 
         if re.match(r'{}-*[0-9]'.format(NoteID) ,item.split('.')[0]):
             input_asset  = os.path.join(dirname+'/image',item)
@@ -46,15 +45,14 @@ def get_asset( dirname ,NoteID):
 
 
 if __name__ == '__main__': 
-    # set variables 
-    dirname      = os.path.dirname(arg.file) # /xxxx/xx
-    filename     = (arg.file).split('/')[-1] # /xxxx/xx/yyy.ipynb => yyy.ipynb
+    dirname      = os.path.dirname(arg.file) # /xx/xx
+    filename     = (arg.file).split('/')[-1] # /xx/xx/yyy.ipynb > yyy.ipynb
     class_name   = filename.split('-')[0]
-    assert len(filename.split('.'))==2
-    NoteID , filetype   = filename.split('.') # NoteID = yyy
 
     assert os.path.isdir('content/{}'.format(class_name))==True
+    assert len(filename.split('.'))==2
 
+    NoteID , filetype   = filename.split('.') # NoteID = yyy
     output_file  = 'content/{}/{}'.format(class_name,filename)
 
     # cp the articles to content 
