d0a0a624a8608046e69bd411b73696b976751291
KentChun33333 <kentchun33333@gmail.com>
2017-05-18 23:18:02 +0800

    up

--- fast-publish.py
+++ fast-publish.py
@@ -6,45 +6,45 @@
  - className should be correnspoding to content/folderName/
  - xx and name should be in one-to-one relation
  - name must not contain - symbol
- - xx in [00,99] 
+ - xx in [00,99]
  - where we call className-xx-name == NoteID
 
 - Asset Identity Encoding Regulation : className-xx-name-yy == NoetID-yy
  - yy in [00,99]
- - yy is the number on the condition of Asset Type, for example : 
+ - yy is the number on the condition of Asset Type, for example :
    - RL-01-MDP-00.jpg
    - RL-01-MDP-00.avi
- 
-- Asset Type 
- - img (jpg | png) 
+
+- Asset Type
+ - img (jpg | png)
  - video (avi with h264 codec) better via youtube or other CDN
  - audio (mp3) better via youtube or other CDN
- 
+
 """
 
 import argparse, os, re
 
 # get cli-variables as input address
 parse = argparse.ArgumentParser()
-parse.add_argument('-f', '--file', required=True, 
+parse.add_argument('-f', '--file', required=True,
     help='the full address of the notebook file')
 arg = parse.parse_args()
 
-# Functions : get corresponding asset (one Note to many Assets) 
-def get_asset( dirname ,NoteID): 
+# Functions : get corresponding asset (one Note to many Assets)
+def get_asset( dirname ,NoteID):
     assert os.path.isdir(dirname+'/image') == True
     assert os.path.isdir('output/image') == True
     file_list = os.listdir(dirname+'/image')
     for item in file_list :
         if item.split('.')[-1] not in ('jpg','png','jepg','bmp','avi'):
-            continue 
+            continue
         if re.match(r'{}-*[0-9]'.format(NoteID) ,item.split('.')[0]):
             input_asset  = os.path.join(dirname+'/image',item)
             output_asset = os.path.join('output/image',item)
             yield input_asset, output_asset
 
 
-if __name__ == '__main__': 
+if __name__ == '__main__':
     dirname      = os.path.dirname(arg.file) # /xx/xx
     filename     = (arg.file).split('/')[-1] # /xx/xx/yyy.ipynb > yyy.ipynb
     class_name   = filename.split('-')[0]
@@ -55,17 +55,17 @@ if __name__ == '__main__':
     NoteID , filetype   = filename.split('.') # NoteID = yyy
     output_file  = 'content/{}/{}'.format(class_name,filename)
 
-    # cp the articles to content 
+    # cp the articles to content
     os.system('cp {} {}'.format(arg.file, output_file))
 
     # cp the assets
-    for input_asset, output_asset in get_asset(dirname, NoteID):  
+    for input_asset, output_asset in get_asset(dirname, NoteID):
         os.system('cp {} {}'.format(input_asset, output_asset))
 
     # git auto, use git to rollback
     os.system('../git_auto')
-    print '[*] please adding ipynb-meta file'
-    print '[*] then pelican -> output git push'
+    print ('[*] please adding ipynb-meta file')
+    print ('[*] then pelican -> output git push')
 
 
 
