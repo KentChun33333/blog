34cb9162c904cee97c3139cdeb325bd0a12e469e
KentChiu_lamresearch <kentchun33333@gmail.com>
2020-05-23 16:02:57 +0800

    update post

--- fast_publish_win10_001.py
+++ fast_publish_win10_001.py
@@ -3,7 +3,7 @@ import os, sys
 cmd = os.system
 
 cmd('make html')
-cmd('cp -rf output/* ../KentWebIO')
+cmd('cp -r output/* ../KentWebIO')
 cmd('cd ../KentWebIO')
 cmd('git add .')
 cmd('git commit -m"update post"')
