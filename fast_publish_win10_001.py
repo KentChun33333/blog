0021d5e1ce52040cdbabca5e0885de001190fab1
KentChiu_lamresearch <kentchun33333@gmail.com>
2020-05-23 16:14:29 +0800

    update post

--- fast_publish_win10_001.py
+++ fast_publish_win10_001.py
@@ -4,9 +4,12 @@ cmd = os.system
 
 cmd('make html')
 cmd('cp -r output/* ../KentWebIO')
-cmd('cd ../KentWebIO')
+#cmd('cd ../KentWebIO')
+cmd('git -C ../KentWebIO add .')
+cmd('git -C ../KentWebIO commit -m"update post"')
+cmd('git -C ../KentWebIO push')
+
 cmd('git add .')
 cmd('git commit -m"update post"')
 cmd('git push')
-
 
